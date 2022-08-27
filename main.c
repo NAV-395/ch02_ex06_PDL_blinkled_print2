5a4aa8a8621a413105d4835cccf995b2cbc730d6
nestorelcrack <nestorantoniovaquera@gmail.com>
2022-08-27 18:28:57 -0300

    elimine la funcion sprintf

--- main.c
+++ main.c
@@ -56,17 +56,17 @@ int main(void)
     /* Configure and enable the UART */
     Cy_SCB_UART_Init(UART_HW, &UART_config, &UART_context);
     Cy_SCB_UART_Enable(UART_HW);
-    char buffer[256];
+    //char buffer[256];
     for (;;)
     {
     	Cy_GPIO_Write(CYBSP_USER_LED_PORT, CYBSP_USER_LED_NUM,0);
     	Cy_SysLib_Delay(250);
-    	sprintf(buffer, "ENCENDIDO \r\n");
-        Cy_SCB_UART_PutString(UART_HW, buffer);
+    	//sprintf(buffer, "ENCENDIDO \r\n");
+        Cy_SCB_UART_PutString(UART_HW, "ENCENDIDO \r\n");
     	Cy_GPIO_Write(CYBSP_USER_LED_PORT, CYBSP_USER_LED_NUM,1);
     	Cy_SysLib_Delay(250);
-    	sprintf(buffer, "APAGADO \r\n");
-        Cy_SCB_UART_PutString(UART_HW, buffer);
+    	//sprintf(buffer, "APAGADO \r\n");
+        Cy_SCB_UART_PutString(UART_HW, "APAGADO \r\n");
     }
 }
 
